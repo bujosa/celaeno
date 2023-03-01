a5dbb000dac8701ca8220c032e8ab7a8568411d1
bujosa <davidbujosa@gmail.com>
2023-03-01 00:52:15 -0400

    chore: report web vitals

--- app/src/index.js
+++ app/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -19,8 +18,3 @@ if (!window.ethereum) {
     </React.StrictMode>
   );
 }
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
